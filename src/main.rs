9685c904d21fbd70cfdaaa0f787d898f7bf095a5
Sean Joseph <theseanjoseph@gmail.com>
2025-07-30 23:19:10 -0400

    Implement hello world serial

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::{hal::Atmega, usart::UsartOps, Usart};
+
 use panic_halt as _;
+
+fn write_usart<B: UsartOps<Atmega, P, T>, P, T>(usart: &mut Usart<B, P, T>, bytes: &[u8]) {
+    for b in bytes {
+        usart.write_byte(*b);
+    }
+}
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let mut usart =    arduino_hal::default_serial!(dp, pins, 57600);
 
     let mut led = pins.d13.into_output();
 
     loop {
         led.toggle();
         arduino_hal::delay_ms(1000);
-    }
+        write_usart(&mut usart, b"Hello, World!\n" );     }
 }
