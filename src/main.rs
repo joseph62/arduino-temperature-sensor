6df04f7b64a86b195ff4291d5d2e10bb083f2568
Sean Joseph <theseanjoseph@gmail.com>
2025-07-30 23:45:37 -0400

    Implement writer wrapper for serial

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::{hal::Atmega, usart::UsartOps, Usart};
+use core::fmt::Write;
+
+use arduino_hal::{
+    Usart,
+    hal::Atmega,
+    usart::{UsartOps, UsartWriter},
+};
 
 use panic_halt as _;
 
-fn write_usart<B: UsartOps<Atmega, P, T>, P, T>(usart: &mut Usart<B, P, T>, bytes: &[u8]) {
-    for b in bytes {
-        usart.write_byte(*b);
+struct SerialWriter<USART: arduino_hal::usart::UsartOps<Atmega, RX, TX>, RX, TX>(
+    Usart<USART, RX, TX>,
+);
+
+impl<USART: UsartOps<Atmega, RX, TX>, RX, TX> SerialWriter<USART, RX, TX> {
+    pub fn new(usart: Usart<USART, RX, TX>) -> Self {
+        Self(usart)
+    }
+}
+
+impl<USART: UsartOps<Atmega, RX, TX>, RX, TX> Write for SerialWriter<USART, RX, TX> {
+    fn write_str(&mut self, str: &str) -> core::fmt::Result {
+        for b in str.as_bytes() {
+            self.0.write_byte(*b);
+        }
+        Ok(())
     }
 }
 
@@ -15,12 +34,13 @@ fn write_usart<B: UsartOps<Atmega, P, T>, P, T>(usart: &mut Usart<B, P, T>, byte
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let mut usart =    arduino_hal::default_serial!(dp, pins, 57600);
+    let mut usart = SerialWriter::new(arduino_hal::default_serial!(dp, pins, 57600));
 
     let mut led = pins.d13.into_output();
 
     loop {
         led.toggle();
         arduino_hal::delay_ms(1000);
-        write_usart(&mut usart, b"Hello, World!\n" );     }
+        write!(usart, "Hello, World!\n");
+    }
 }
